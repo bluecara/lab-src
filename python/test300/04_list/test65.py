bd89e32e1b540a096f65e88e2439537fa5456398
Kevin Jung <bluecara82@gmail.com>
2024-03-21 15:17:29 +0900

    Create files

---
+++ python/test300/04_list/test65.py
@@ -0,0 +1,5 @@
+# interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+
+interest = ['삼성전자', 'LG전자', 'Naver']
+print(interest[0], interest[2])
+# 삼성전자 Naver
---
+++ python/test300/04_list/test69.py
@@ -0,0 +1,6 @@
+# 회사 이름이 슬래시 ('/')로 구분되어 하나의 문자열로 저장되어 있다.
+
+string = "삼성전자/LG전자/Naver"
+interest = string.split("/")
+print(interest)
+# ['삼성전자', 'LG전자', 'Naver']
